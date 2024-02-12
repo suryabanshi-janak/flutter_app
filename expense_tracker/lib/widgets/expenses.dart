16a610bfb94118de9e1be6d1fd419edf158dc133
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-02-12 09:10:12 +0545

    expense tracker initial commit

---
+++ expense_tracker/lib/widgets/expenses.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _Expenses();
+  }
+}
+
+class _Expenses extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('Chart'),
+          Expanded(
+            child: ExpenseList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
