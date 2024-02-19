5362c7c380fb8217db3933a069a47ff4d03273d6
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-02-19 11:46:42 +0545

    meals_app: drawer and providers

--- meals_app/lib/screens/meal_details.dart
+++ meals_app/lib/screens/meal_details.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/providers/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
+class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
@@ -11,10 +13,29 @@ class MealDetailsScreen extends StatelessWidget {
   final Meal meal;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isFavorite = ref.watch(favoriteMealsProvider).contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                      wasAdded ? 'Meal added as a favorite.' : 'Meal removed.'),
+                ),
+              );
+            },
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
--- meals_app/lib/screens/tabs.dart
+++ meals_app/lib/screens/tabs.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/favorites_provider.dart';
 
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
-class Tabs extends StatefulWidget {
+class Tabs extends ConsumerStatefulWidget {
   const Tabs({super.key});
 
   @override
-  State<Tabs> createState() {
+  ConsumerState<Tabs> createState() {
     return _Tabs();
   }
 }
 
-class _Tabs extends State<Tabs> {
+class _Tabs extends ConsumerState<Tabs> {
   int _selectedPageIndex = 0;
 
   void _selectPage(index) {
@@ -23,21 +26,23 @@ class _Tabs extends State<Tabs> {
 
   @override
   Widget build(BuildContext context) {
-    Widget content = const CategoriesScreen();
-    String title = 'Categories';
+    Widget activePage = const CategoriesScreen();
+    String activePageTitle = 'Categories';
 
     if (_selectedPageIndex == 1) {
-      content = const MealsScreen(
-        meals: [],
+      final favoriteMeals = ref.watch(favoriteMealsProvider);
+      activePage = MealsScreen(
+        meals: favoriteMeals,
       );
-      title = 'Favourites';
+      activePageTitle = 'Favourites';
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(activePageTitle),
       ),
-      body: content,
+      drawer: const MainDrawer(),
+      body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         currentIndex: _selectedPageIndex,
