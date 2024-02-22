8223955d0ebc16eae9e736519b5ba5bd2120c651
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-02-22 10:09:37 +0545

    shopping_list_app: sending http request

--- shopping_list_app/lib/widgets/new_item.dart
+++ shopping_list_app/lib/widgets/new_item.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
@@ -18,12 +22,33 @@ class _NewItem extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     _formKey.currentState!.validate();
     _formKey.currentState!.save();
+
+    final url = Uri.https('flutter-shopping-9e6b4-default-rtdb.firebaseio.com',
+        'shopping-list.json');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'name': _enteredName,
+        'quantity': _enteredQuantity,
+        'category': _selectedCategory.title,
+      }),
+    );
+
+    final Map<String, dynamic> resData = json.decode(response.body);
+
+    if (!context.mounted) {
+      return;
+    }
+
     Navigator.of(context).pop(
       GroceryItem(
-        id: DateTime.now().toString(),
+        id: resData['name'],
         name: _enteredName,
         quantity: _enteredQuantity,
         category: _selectedCategory,
