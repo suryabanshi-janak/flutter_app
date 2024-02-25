df10e81d2e4d8eeee6ad9db55135404c7cc56753
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-02-25 09:32:57 +0545

    favorite_places_app: storing data locally on device

--- favorite_places_app/lib/models/place.dart
+++ favorite_places_app/lib/models/place.dart
@@ -8,7 +8,8 @@ class Place {
   Place({
     required this.title,
     required this.image,
-  }) : id = uuid.v4();
+    String? id,
+  }) : id = id ?? uuid.v4();
 
   final String id;
   final String title;
--- favorite_places_app/lib/screens/places.dart
+++ favorite_places_app/lib/screens/places.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:favorite_places_app/providers/user_places.dart';
 import 'package:favorite_places_app/screens/add_place.dart';
 import 'package:favorite_places_app/widgets/place_list.dart';
+import 'package:favorite_places_app/providers/user_places.dart';
 
 class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  ConsumerState<PlacesScreen> createState() => _PlacesScreenState();
+  ConsumerState<PlacesScreen> createState() {
+    return _PlacesScreenState();
+  }
 }
 
 class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final places = ref.watch(userPlacesProvider);
+    final userPlaces = ref.watch(userPlacesProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
         actions: [
           IconButton(
+            icon: const Icon(Icons.add),
             onPressed: () {
               Navigator.of(context).push(
                 MaterialPageRoute(
@@ -29,14 +40,19 @@ class _PlacesScreenState extends ConsumerState<PlacesScreen> {
                 ),
               );
             },
-            icon: const Icon(Icons.add),
           ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: PlaceList(
-          places: places,
+        child: FutureBuilder(
+          future: _placesFuture,
+          builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(child: CircularProgressIndicator())
+                  : PlaceList(
+                      places: userPlaces,
+                    ),
         ),
       ),
     );
