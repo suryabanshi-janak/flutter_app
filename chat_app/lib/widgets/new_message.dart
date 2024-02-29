03087256c59f6fc00886c201cb4e84c2712a6239
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-02-29 22:27:12 +0545

    chat_app: message and new chat

---
+++ chat_app/lib/widgets/new_message.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _NewMessage();
+  }
+}
+
+class _NewMessage extends State<NewMessage> {
+  var _messageController = TextEditingController();
+
+  void _submitMessage() {
+    final message = _messageController.text;
+
+    if (message.trim().isEmpty) {
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              autocorrect: true,
+              enableSuggestions: true,
+              textCapitalization: TextCapitalization.sentences,
+              decoration: const InputDecoration(labelText: 'Send a message...'),
+            ),
+          ),
+          IconButton(
+            onPressed: _submitMessage,
+            icon: const Icon(Icons.send),
+            color: Theme.of(context).colorScheme.primary,
+          )
+        ],
+      ),
+    );
+  }
+}
